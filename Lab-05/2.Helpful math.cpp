70fba259b62b525e6ba85e02e0059d6e5297861b
Md. Shahin Hossain <139230735+shahin-010@users.noreply.github.com>
2023-08-08 16:13:30 +0600

    Update 2.Helpful math.cpp

--- Lab-05/2.Helpful math.cpp
+++ Lab-05/2.Helpful math.cpp
@@ -1,28 +1,36 @@
-#include<iostream>
-#include <bits/stdc++.h>
-
+#include<bits/stdc++.h>
+#include<map>
+#include <algorithm>
+#include<vector>
+#include<map>
+#include<string>
+ 
 using namespace std;
-
-int main(){
-
-    string arr;
-    cin>>arr;
-    int newarr[1001];
-
-    int count=0;
-    for(int i=0;i<arr.size();i++){
-        if(arr[i]=='+'){
+ 
+int main()
+{
+    int cnt=0;
+    string s;
+    cin>>s;
+    int b[1000];
+    for(int i=0;i<s.length();i++){
+        if(s[i]=='+'){
             continue;
-        }else{
-            newarr[count++]=arr[i]-'0';
+        }
+        else{
+            b[cnt]=s[i]-'0';
+            cnt++;
         }
     }
-    sort(newarr,newarr+count);
-    for(int i=0;i<=count;i++){
-    cout<<newarr[i];
-    if(i==count-1)
-        break;
-    cout<<'+';
+    sort(b,b+cnt);
+    for(int i=0;i<cnt;i++){
+        cout<<b[i];
+        if(i==cnt-1){
+            break;
+        }
+        cout<<"+";
     }
-return 0;
+ 
+ 
+    return 0;
 }
