5491d40ba02267c1f452edfb1c1f94d54fda70fe
Md. Shahin Hossain <139230735+shahin-010@users.noreply.github.com>
2023-08-08 16:16:49 +0600

    Update 1. Beautiful Matrix.cpp

--- Lab-05/1. Beautiful Matrix.cpp
+++ Lab-05/1. Beautiful Matrix.cpp
@@ -1,22 +1,23 @@
-    #include <iostream>
-    #include <cmath>
-     
-    using namespace std;
-     
-    int main()
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int x ;
+    int res;
+    for (int i = 1; i <= 5; ++i)
     {
-        int x = 0;
-        for (int i = 1; i <= 5; ++i)
+        for (int j = 1; j <= 5; ++j)
         {
-            for (int j = 1; j <= 5; ++j)
+            cin >> x;
+            if (x == 1)
             {
-                cin >> x;
-                if (x == 1)
-                {
-                    cout << abs(i - 3) + abs(j - 3) << endl;
-                }
+                res= abs(i - 3) + abs(j - 3) ;
             }
         }
-        return 0;
     }
-
+    cout<<res;
+    return 0;
+}
